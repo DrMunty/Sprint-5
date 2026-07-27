7edbb9163eb5ce835ddbbed5302bdd43682a68e8
albert-dev <al96@hotmail.es>
2026-07-27 13:17:24 +0200

    feat: "the user can access the book details and return to the list via routes"

--- src/app/components/book-details/book-details.ts
+++ src/app/components/book-details/book-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
+import { BookService } from '../../services/book-service';
+import type { Book } from '../../models/book-interface';
 
 @Component({
   selector: 'app-book-details',
@@ -6,4 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './book-details.html',
   styleUrl: './book-details.css',
 })
-export class BookDetails {}
+export class BookDetails {
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router)
+  private bookService = inject(BookService)
+  bookId = this.activatedRoute.snapshot.params['bookId'];
+
+  book: Book | undefined = this.bookService.myBooks().find(b => b.id === this.bookId)
+
+  returnToList(){
+    this.router.navigate(['/books']);
+  }
+}
