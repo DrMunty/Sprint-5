17029ff0335c1ddb463f6829de5f7a2a6f514ad3
albert-dev <al96@hotmail.es>
2026-07-27 11:41:03 +0200

    feat: "created basic routing structure for the project"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { BookList } from './components/book-list/book-list';
+import { BookDetails } from './components/book-details/book-details';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home},
+    { path: 'redirectHome', redirectTo: '', pathMatch: 'prefix'},
+    { path: 'books', component: BookList},
+    { path: 'books/:bookId', component: BookDetails},
+    { path: '**', component: NotFound}
+];
